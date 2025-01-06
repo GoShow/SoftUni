32d08c04411789486f32ff51f4285f57f0a13e24
Georgi Inkov <georgi.inkov@scaleforce.io>
2025-01-06 15:21:01 +0200

    more

---
+++ CSharp-Advanced-May-2023/10.ExamPreparation/03.SoftUniKindergarten/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SoftUniKindergarten;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        var aaa = "asd";
+
+
+        Do(aaa);
+
+        Console.WriteLine(aaa);
+        void Do(string bbb)
+        {
+            bbb += "v";
+            Console.WriteLine();
+        }
+    }
+}
