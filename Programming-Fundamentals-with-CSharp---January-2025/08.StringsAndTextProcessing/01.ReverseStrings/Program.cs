cccf6edee8005ad1192cb62fc76e49fdbb4ed103
Georgi Inkov <georgi.inkov@scaleforce.io>
2025-03-05 21:28:05 +0200

    Text processing

---
+++ Programming-Fundamentals-with-CSharp---January-2025/08.StringsAndTextProcessing/01.ReverseStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ReverseStrings;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string stringToReverse = null;
+
+        while ((stringToReverse = Console.ReadLine()) != "end")
+        {
+            string reversedString = string.Join("", stringToReverse.Reverse());
+
+            Console.WriteLine($"{stringToReverse} = {reversedString}");
+        }
+    }
+}
